56499e591a04805e509bb97ee1364442156c71ee
Chelsea <chelsea@tripwiretech.com>
2022-12-17 10:30:34 +1300

    Expo location

--- App.js
+++ App.js
@@ -1,10 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, TextInput, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [location, setLocation] = useState();
+  const [address, setAddress] = useState();
+
+  Location.setGoogleApiKey("AIzaSyD5GUOMMrDY5Ml8JOQ5j7z7p9f8GaGCDBg");
+
+  useEffect(() => {
+    const getPermissions = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log("Please grant location permissions");
+        return;
+      }
+
+      let currentLocation = await Location.getCurrentPositionAsync({});
+      setLocation(currentLocation);
+      console.log("Location:");
+      console.log(currentLocation);
+    };
+    getPermissions();
+  }, []);
+
+  const geocode = async () => {
+    const geocodedLocation = await Location.geocodeAsync(address);
+    console.log("Geocoded Address:");
+    console.log(geocodedLocation);
+  };
+
+  const reverseGeocode = async () => {
+    const reverseGeocodedAddress = await Location.reverseGeocodeAsync({
+      longitude: location.coords.longitude,
+      latitude: location.coords.latitude
+    });
+
+    console.log("Reverse Geocoded:");
+    console.log(reverseGeocodedAddress);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput placeholder='Address' value={address} onChangeText={setAddress} />
+      <Button title="Geocode Address" onPress={geocode} />
+      <Button title="Reverse Geocode Current Location" onPress={reverseGeocode} />
       <StatusBar style="auto" />
     </View>
   );
